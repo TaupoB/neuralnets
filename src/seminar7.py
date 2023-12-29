654b3e50c7b35886aee95568da5272537a3cebb7
Aydar Tair <javard@ya.ru>
2023-12-29 15:14:42 +0300

    seminar7

--- src/seminar7.py
+++ src/seminar7.py
@@ -21,8 +21,8 @@ PATH_TO_TRAIN_DATA = 'data/raw/spam.csv'
 PATH_TO_TEST_DATA = 'data/raw/spam_test.csv'
 PATH_TO_MODEL = 'models/model_7'
 BUCKET_NAME = 'neuralnets2023'
-# todo fix your git user name
-YOUR_GIT_USER = 'labintsev'
+
+YOUR_GIT_USER = 'TaupoB'
 
 
 def download_data():
@@ -38,12 +38,14 @@ def download_data():
 def make_model():
     """
     Make recurrent model for binary classification.
-    todo find good layers and hyperparameters
     :return:
     """
     inputs = tf.keras.layers.Input(name='inputs', shape=[MAX_SEQ_LEN])
     x = tf.keras.layers.Embedding(MAX_WORDS, output_dim=4, input_length=MAX_SEQ_LEN)(inputs)
-    x = tf.keras.layers.SimpleRNN(units=4)(x)
+    x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, activation='tanh', return_sequences=True))(x)
+    x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64))(x)
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dense(32, activation='relu')(x)
     x = tf.keras.layers.Dense(1, name='out_layer')(x)
     x = tf.keras.layers.Activation('sigmoid')(x)
     recurrent_model = tf.keras.Model(inputs=inputs, outputs=x)
